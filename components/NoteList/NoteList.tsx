ef06a3279fb839b5d3060905284dad4919d66933
anastasiia_obolonska <velentiy@gmail.com>
2025-08-03 15:37:56 +0300

    fix

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -20,20 +20,22 @@ export default function NoteList({ notes }: NoteListProps) {
   return (
     <ul className={css.list}>
       {notes.map((note) => (
-        <Link href={`/notes/${note.id}`} key={note.id} className={css.listItem}>
+        <li key={note.id} className={css.listItem}>
           <h2 className={css.title}>{note.title}</h2>
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
-            <span className={css.tag}>{note.tag}</span>
+            {note.tag && <span className={css.tag}>{note.tag}</span>}
+            <Link className={css.link} href={`/notes/${note.id}`}>
+              View details
+            </Link>
             <button
-              type="button"
               className={css.button}
               onClick={() => deleteNoteMutation.mutate(note.id)}
             >
               Delete
             </button>
           </div>
-        </Link>
+        </li>
       ))}
     </ul>
   );
